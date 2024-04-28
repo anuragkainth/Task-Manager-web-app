7f20a388ff3a83f47c452c52095ed6c12889f3e3
Anurag Kainth <anuragkainth07@gmail.com>
2024-04-28 16:50:41 +0530

    Backend-added

---
+++ server/models/taskModel.js
@@ -0,0 +1,22 @@
+// taskModel.js
+
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ""
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+module.exports = Task;
---
+++ server/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const taskRoutes = require("../server/routes/taskRoutes");
+
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/tasks", taskRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Connected to DB & listening on port:", PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to DB:", error.message);
+  });
